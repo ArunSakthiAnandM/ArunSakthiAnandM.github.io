28e6acd730d5591999d8f2e1ea207f4e3e32bb79
Arun Sakthi Anand M <arunsakthianand@gmail.com>
2024-06-15 21:56:59 +0530

    Added router and updated header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,9 @@ import { HomeComponent } from './components/home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent
-  ],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Arun Sakthi Anand';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
+import { TeachingsComponent } from './components/teachings/teachings.component';
+import { CvComponent } from './components/cv/cv.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'', redirectTo: 'home', pathMatch: 'full' },
+    { path:'home', component:  HomeComponent },
+    { path:'blogs', component: BlogsComponent },
+    { path:'teachings', component: TeachingsComponent },
+    { path:'cv', component: CvComponent },
+];
