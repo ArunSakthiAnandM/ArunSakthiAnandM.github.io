d6935aeee3d59e9ba8fcccdcdccc8e323346371a
Arun Sakthi Anand M <arunsakthianand@gmail.com>
2024-06-15 21:56:15 +0530

    Added new components

---
+++ src/app/components/teachings/teachings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teachings',
+  standalone: true,
+  imports: [],
+  templateUrl: './teachings.component.html',
+  styleUrl: './teachings.component.css'
+})
+export class TeachingsComponent {
+
+}
