19d27f6904c9beadfe3098530fbc566084ea1bf2
Arun Sakthi Anand M <arunsakthianand@gmail.com>
2024-06-17 20:53:07 +0530

    Updated Skills

---
+++ src/app/components/home/skills/skills.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skills',
+  standalone: true,
+  imports: [],
+  templateUrl: './skills.component.html',
+  styleUrl: './skills.component.css',
+})
+export class SkillsComponent {
+  skillsList = [
+    {
+      title: 'Programming Languages',
+      skills: [
+        'C',
+        'C++',
+        'Java',
+        'JavaScript',
+        'Typescript',
+        'HTML',
+        'CSS',
+        'Python',
+      ],
+    },
+    {
+      title: 'Frameworks',
+      skills: [
+        'Spring',
+        'Spring boot',
+        'Angular',
+        'Otter',
+        'StencilJS',
+        'React',
+        'NodeJS',
+        'ExpressJS',
+      ],
+    },
+    {
+      title: 'Tools and Applications',
+      skills: [
+        'PostgreSQL',
+        'MongoDB',
+        'Firebase',
+        'Supabase',
+        'Android Studio',
+        'Git',
+        'Github',
+        'LaTeX',
+      ],
+    },
+  ];
+}
